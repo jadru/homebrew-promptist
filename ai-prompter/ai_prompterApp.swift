0a04e90f55d634afafddfc758f354e50dc2f6a3e
Younggun Park <jadru@p2ach.ai>
2025-11-25 23:09:32 +0900

    Initial Commit

---
+++ ai-prompter/ai_prompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_prompterApp.swift
+//  ai-prompter
+//
+//  Created by Younggun Park on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ai_prompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
